b011e04478ba22caa4b314dc50a8eb883fa7a06f
Oliwerix <oliwerix@gmail.com>
2023-04-17 16:10:42 +0200

    comit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,16 @@
     import {slike } from './slike'    
 
 </script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    img {
+        width: 100px;
+    }
+</style>
+<h1>Slike maturantskega plesa GIMB 2023</h1>
 
 {#each slike as slika}
     <a href="{slika.split(".")[0]}.jpg">
-        <img src={`${slika}`} alt="slika iz maturantskega izleta z imenom {slika}, katera je na tem strezniku LP webmaster."/>
+        <img src={`${slika}`} loading="lazy" alt="slika iz maturantskega izleta z imenom {slika}, katera je na tem strezniku LP webmaster."/>
 
     </a>
 {/each}
